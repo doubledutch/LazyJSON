1fc7bba2785f7ecf319e1018c340f672c210669d
Kasper Jeppe Jeppesen <kasper.jeppesen@gmail.com>
2016-08-12 14:02:26 -0400

    add negative test case

--- src/test/java/me/doubledutch/lazyjson/compressor/TemplateExtractionTest.java
+++ src/test/java/me/doubledutch/lazyjson/compressor/TemplateExtractionTest.java
@@ -17,6 +17,16 @@ public class TemplateExtractionTest{
 
     	Template t1=obj1.extractTemplate();
     	Template t2=obj2.extractTemplate();
-    	assertTrue(t1.equals(t2));
+    	assertEquals(t1,t2);
+    }
+
+    @Test
+    public void differentTemplates(){
+    	LazyObject obj1=new LazyObject("{\"foo\":\"bar\"}");
+    	LazyObject obj2=new LazyObject("{\"foo\":\"Hello World!\",\"baz\":\"42\"}");
+
+    	Template t1=obj1.extractTemplate();
+    	Template t2=obj2.extractTemplate();
+    	assertNotEquals(t1,t2);
     }
 }
